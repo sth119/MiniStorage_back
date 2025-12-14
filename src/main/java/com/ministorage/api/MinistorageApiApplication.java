efd01d7dbacd2b7d99c5039c773bec39064dacc7
sth119 <08aaa80@naver.com>
2025-12-14 18:06:16 +0900

    setting

---
+++ src/main/java/com/ministorage/api/MinistorageApiApplication.java
@@ -0,0 +1,13 @@
+package com.ministorage.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinistorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinistorageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ministorage/api/MinistorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ministorage.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinistorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
